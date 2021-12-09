d194845a1499b40d9dde128828cdc827eec30a56
Arginine3 <ym.love.1003@icloud.com>
2021-12-09 16:55:47 +0900

    STEP: 4 配列の入力 1
    正解できた

---
+++ array_menu/step4_配列の入力1.php
@@ -0,0 +1,9 @@
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    $input_line = [8, 1, 3, 3, 8, 1, 1, 3, 8, 8];
+    foreach($input_line as $val){
+        $vals = $val."\n";
+        echo $vals;
+    }
+?>
